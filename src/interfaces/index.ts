2112905f915fd6fc7e8f2c4b6a0a5e7161532c14
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-05-05 15:49:22 +0200

    Bump version to 1.1.2 and export MarkdownTypewriterHooks in index files

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,1 +1,2 @@
-export { default as MarkdownTypewriterProps } from "./MarkdownTypewriterProps";
+export { MarkdownTypewriterHooksProps, default as MarkdownTypewriterProps } from "./MarkdownTypewriterProps";
+
