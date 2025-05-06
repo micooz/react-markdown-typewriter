867e17977c24a3aca3cf99d43222decb20bffa12
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-05-06 20:35:02 +0200

    Add emoji detection in TypewriterItem for enhanced character handling

--- src/components/TypewriterItem.tsx
+++ src/components/TypewriterItem.tsx
@@ -11,6 +11,11 @@ function throttle(func: (...args: any[]) => void, limit: number) {
         }
     };
 }
+// TODO: check if exists a library for this
+function isEmoji(str: string) {
+    const emojiRegex = /([\u203C-\u3299]|[\uD83C-\uDBFF\uDC00-\uDFFF]|[\u2600-\u26FF]|[\u2700-\u27BF])/g;
+    return emojiRegex.test(str);
+}
 
 export default function TypewriterItem({
     children,
@@ -28,6 +33,25 @@ export default function TypewriterItem({
     key?: Key | null | undefined;
 }) {
     if (typeof children === "string") {
+        // check if is emoji
+        if (isEmoji(children)) {
+            const ref = useRef<HTMLSpanElement>(null);
+            const onAnimationComplete = onCharacterAnimationComplete
+                ? throttle(() => onCharacterAnimationComplete(ref), 10)
+                : undefined;
+            const component = (
+                <motion.span
+                    ref={ref}
+                    className={className}
+                    key={`span-${key}-${children}`}
+                    variants={characterVariants}
+                    onAnimationComplete={onAnimationComplete}
+                >
+                    {children}
+                </motion.span>
+            );
+            return dadElement(component, true);
+        }
         const spanList = children.split("").map((char, i) => {
             const ref = useRef<HTMLSpanElement>(null);
             const onAnimationComplete = onCharacterAnimationComplete
