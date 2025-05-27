bae0d68b3f4fdb7fa06274d1d5762d10e0114305
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-05-27 12:22:15 +0200

    Update module entry point to use .mjs and enhance exports configuration

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,16 +1,20 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-    entry: ["src/**/*@(ts|tsx)"],
+    entry: ["src/**/index.ts"],
     format: ["cjs", "esm"], // Build for commonJS and ESmodules
     dts: true, // Generate declaration file (.d.ts)
     treeshake: true,
     splitting: false,
-    sourcemap: false, // Generate sourcemap
     clean: true,
     minify: true,
     bundle: true,
     skipNodeModulesBundle: false, // Skip bundling of node_modules
     entryPoints: ["src/index.ts"],
     noExternal: ["motion"],
+    outExtension({ format }) {
+        return {
+            js: format === "esm" ? ".mjs" : ".cjs",
+        };
+    },
 });
