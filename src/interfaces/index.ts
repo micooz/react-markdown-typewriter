118827c9ae96c2efe1db68df35558dd602ff4c7f
micooz <micooz@hotmail.com>
2025-06-15 11:10:29 +0800

    fix: type warnings

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,2 +1,4 @@
-export { MarkdownTypewriterHooksProps, default as MarkdownTypewriterProps } from "./MarkdownTypewriterProps";
-
+export type {
+  MarkdownTypewriterHooksProps,
+  default as MarkdownTypewriterProps,
+} from './MarkdownTypewriterProps';
